2219df2ca6985aa10707248146c9762f7d1b5b0a
ChaitanyaKumar199331 <devopsguru1997@gmail.com>
2026-04-17 15:55:03 +0530

    Update variables.tf

--- Day-1-terraform-basic-code/variables.tf
+++ Day-1-terraform-basic-code/variables.tf
@@ -1,11 +1,11 @@
 variable "ami_id" {
   description = "AMI for ec2 instance"
-  default = "ami-098e39bafa7e7303d"
+  default = ""
   type = string
 }
 
 variable "instance_type" {
   description = "Instance type for ec2 instance"
-  default = "t3.micro"
+  default = ""
   type = string
 }
